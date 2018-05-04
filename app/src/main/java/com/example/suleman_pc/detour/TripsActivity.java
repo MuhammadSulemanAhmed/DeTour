4e647c81fe2a58e9edb3e7065300c1ca67931c60
suleman-pc <suleman217010@gmail.com>
2018-05-05 00:48:30 +0500

    Graphic Designed

--- app/src/main/java/com/example/suleman_pc/detour/TripsActivity.java
+++ app/src/main/java/com/example/suleman_pc/detour/TripsActivity.java
@@ -1,9 +1,11 @@
 package com.example.suleman_pc.detour;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
@@ -13,6 +15,7 @@ import android.widget.Button;
 import android.widget.GridView;
 
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.suleman_pc.detour.Adapter.TripsGridAdapter;
@@ -22,36 +25,37 @@ import com.example.suleman_pc.detour.Model.TripModel;
 
 import java.util.ArrayList;
 
-public class TripsActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
+public class TripsActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     private TripsDatabaseHandler db;
     private GridView lv;
     private TripsGridAdapter data;
     private TripModel dataModel;
-    private Button btn;
-static  int p=0;
-Context context;
+    static int p = 0;
+    TextView trip_amount;
+    Context context;
+    Dialog dailoge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trips);
         db = new TripsDatabaseHandler(this);
+        dailoge = new Dialog(this);
 
         lv = (GridView) findViewById(R.id.list1);
-        btn = findViewById(R.id.addnew);
+        FloatingActionButton btn = findViewById(R.id.addnew);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(TripsActivity.this, AddNewTripActivity.class);
                 startActivity(intent);
-                finish();
             }
         });
 
         ShowRecords();
+
     }
-
-
 
     private void ShowRecords() {
         final ArrayList<TripModel> tripModels = new ArrayList<>(db.getAllContacts());
@@ -85,19 +89,20 @@ Context context;
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 dataModel = tripModels.get(position);
                 ShareData.getInstance().current_TripId = dataModel.getID();
-                Toast.makeText(getApplicationContext(),"",Toast.LENGTH_SHORT).show();
-                Showpopup(view);
+                Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+                Showpopup1(view);
 
                 return true;
 
             }
         });
     }
-        public void Showpopup(View v){
-            PopupMenu popupMenu=new PopupMenu(this,v);
-            popupMenu.setOnMenuItemClickListener(this);
-            popupMenu.inflate(R.menu.trip_long_click_menu);
-            popupMenu.show();
+
+    public void Showpopup1(View v) {
+        PopupMenu popupMenu = new PopupMenu(this, v);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.trip_long_click_menu);
+        popupMenu.show();
 
 
     }
@@ -109,22 +114,36 @@ Context context;
             case R.id.edit_trip:
                 Intent intent = new Intent(TripsActivity.this, EditTripActivity.class);
                 startActivity(intent);
+                break;
             case R.id.delete_trip:
                 dailog();
+                break;
+//            case R.id.end_trip:
+//                endtrip();
+//                break;
             default:
                 return false;
         }
+        return false;
 
     }
-    public void  delete() {
+
+    private void endtrip() {
+        Toast.makeText(this, "Trip ENded", Toast.LENGTH_LONG).show();
+
+    }
+
+
+    public void delete() {
         db.deleteContact(ShareData.getInstance().current_TripId);
         db.deleteAllExpense(ShareData.getInstance().current_TripId);
-        Intent refresh = new Intent(this,  TripsActivity.class);
+        Intent refresh = new Intent(this, TripsActivity.class);
         startActivity(refresh);
         this.finish();
 
     }
-    public void dailog(){
+
+    public void dailog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Delete Trip");
         builder.setMessage("Are you sure to delete trip?");
@@ -133,7 +152,7 @@ Context context;
 
             public void onClick(DialogInterface dialog, int which) {
                 delete();
-                Toast.makeText(getApplicationContext(),"Sucessfully Deleted",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Sucessfully Deleted", Toast.LENGTH_SHORT).show();
                 dialog.dismiss();
             }
         });
@@ -152,4 +171,14 @@ Context context;
         alert.show();
 
     }
-}
+
+//    public void Showpopup(MenuItem item) {
+//        TextView trip;
+//        dailoge.setContentView(R.layout.trip_end_popup);
+//        trip = findViewById(R.id.popup_tripcost);
+////        total();
+//        dailoge.show();
+//
+//
+//    }
+    }
